9647f4c663abddb8d9f2c60df7ce267812d372d2
wu9o <wu9ors@gmail.com>
2025-08-18 00:24:12 +0800

    feat: add base

--- blog/cogita.config.ts
+++ blog/cogita.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     title: 'Cogita, Ergo Sum | 我思，故我在',
     description:
       '在这里，我记录编码、创造与思考的瞬间。Cogita 框架，便是我对“优雅构建”的一次哲学实践。',
+    base: 'cogita',
   },
   themeConfig: {
     socialLinks: [
